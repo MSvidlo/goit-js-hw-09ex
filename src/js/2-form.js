5ee6b597c6f507eefd5cd80a39f9d9101e5259c3
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-01-29 23:05:31 +0100

    ffff

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,9 +1,58 @@
-const STORAGE_KEY = 'feedback-form-state';
+// const STORAGE_KEY = 'feedback-form-state';
 
 
 const form = document.querySelector('.feedback-form');
 const textarea = form.querySelector('textarea');
 
-function onFormInput() {
-    const 
+form.addEventListener('input', onFormInput);
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+    e.preventDefault();
+const name = form.elements.email.value;
+    const message = form.elements.message.value;
+
+    const data = {
+        name,
+        message,
+    };
+
+    form.reset();
+    localStorage.removeItem('feedback-form-state');
 }
+function onFormInput() {
+    const email = form.elements.email.value;
+    const message = form.elements.message.value;
+
+    const data = {
+        email,
+        message,
+    };
+    saveToLS('feedback-form-state', data);
+} 
+
+
+function saveToLS(key, value) {
+    const zip = JSON.stringify(value);
+    localStorage.setItem(key,zip)
+    
+};
+
+function loadFromLS(key) {
+    const zip = localStorage.getItem(key);
+   try {
+   return  JSON.parse(zip)
+   } catch (error) {
+       return zip;
+    
+   }
+    
+}
+
+function init() {
+    const data = loadFromLS('feedback-form-state')|| {};
+    form.elements.email.value = data.email || "";
+    form.elements.message.value = data.message || "";
+    console.log(form.elements);
+}
+init();
