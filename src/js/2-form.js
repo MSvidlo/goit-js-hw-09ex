c19ca11420f1b639ddc2baefe9b48fcb828cf2bd
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-01-29 21:06:15 +0100

    update

---
+++ src/js/2-form.js
@@ -0,0 +1,9 @@
+const STORAGE_KEY = 'feedback-form-state';
+
+
+const form = document.querySelector('.feedback-form');
+const textarea = form.querySelector('textarea');
+
+function onFormInput() {
+    const 
+}
